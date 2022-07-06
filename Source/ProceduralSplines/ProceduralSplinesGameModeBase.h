81c04a587cd7be1aa463b7eeb1c0691be65ad180
Can Kozan <blackclimb34@windowslive.com>
2022-07-06 15:08:37 +0200

    First Commit

---
+++ Source/ProceduralSplines/ProceduralSplinesGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProceduralSplinesGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROCEDURALSPLINES_API AProceduralSplinesGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
